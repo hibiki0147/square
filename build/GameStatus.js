50c284831cd5f95ca8c20853e9fed0975b0f0435
hibiki0147 <hibi.king.red@gmail.com>
2024-08-25 11:51:47 +0900

    ゲームの状態を追加

---
+++ build/GameStatus.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GameStatus = void 0;
+var GameStatus;
+(function (GameStatus) {
+    GameStatus[GameStatus["Progress"] = 0] = "Progress";
+    GameStatus[GameStatus["End"] = 1] = "End";
+})(GameStatus || (exports.GameStatus = GameStatus = {}));
+//# sourceMappingURL=GameStatus.js.map
