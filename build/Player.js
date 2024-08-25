7ca2ad779d77c732ce56229d65f21f27d98822b8
hibiki0147 <hibi.king.red@gmail.com>
2024-08-25 11:17:11 +0900

    ゲームに関する状態の宣言

---
+++ build/Player.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Player = void 0;
+var Player;
+(function (Player) {
+    Player[Player["red"] = 0] = "red";
+    Player[Player["Blue"] = 1] = "Blue";
+})(Player || (exports.Player = Player = {}));
+//# sourceMappingURL=Player.js.map
---
+++ src/LineStatus.ts
@@ -0,0 +1,5 @@
+export enum LineStatus {
+  Red,
+  Blue,
+  None
+}
