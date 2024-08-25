df399f68f3d41ef41865e2ead2179af2a8c18018
hibiki0147 <hibi.king.red@gmail.com>
2024-08-25 12:00:11 +0900

    Gameに状態の宣言を反映

--- build/Game.js
+++ build/Game.js
@@ -1,7 +1,12 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Player_1 = require("./Player");
+const GameStatus_1 = require("./GameStatus");
 class Game {
     constructor() {
-        this.turnPlayer = 1;
+        this.turnPlayer = Player_1.Player.Red;
+        this.gameStatus = GameStatus_1.GameStatus.Progress;
+        this.gameResult = null;
     }
 }
 //# sourceMappingURL=Game.js.map
--- src/Game.ts
+++ src/Game.ts
@@ -1,8 +1,16 @@
+import { Player } from "./Player";
+import { GameStatus } from "./GameStatus";
+import { GameResult } from "./GameResult";
 
 class Game {
-  private turnPlayer:number = 1;
+  private turnPlayer:Player;
+  private gameStatus:GameStatus;
+  private gameResult:GameResult | null;
 
   constructor() {
-    
+    this.turnPlayer = Player.Red;
+    this.gameStatus = GameStatus.Progress;
+    this.gameResult = null;
   }
+  
 }
