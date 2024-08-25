a5ac93b04ac9bd50aa62d9dca788c5629fc4e164
hibiki0147 <hibi.king.red@gmail.com>
2024-08-25 13:01:22 +0900

    勝利パターンのドメイン宣言

---
+++ build/VictoryPattern.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VictoryPattern = void 0;
+class VictoryPattern {
+    constructor() {
+        this.shapeDataList = [
+            [0, 1, 2],
+            [3, 13, 23],
+            [8, 16, 24],
+            [0, 3, 5, 10],
+            [0, 1, 3, 7, 13, 17, 20, 21],
+            [0, 1, 3, 7, 10, 11],
+            [0, 3, 5, 13, 15, 20],
+            [0, 1, 3, 6, 10],
+            [1, 4, 7, 10, 11],
+            [4, 5, 13, 15, 20],
+            [0, 3, 5, 13, 14],
+            [1, 4, 6, 13, 14],
+            [6, 7, 14, 16, 20],
+            [1, 4, 6, 10],
+            [2, 6, 8, 14, 16, 20],
+            [1, 2, 4, 8, 10, 11],
+        ];
+    }
+    check() {
+    }
+}
+exports.VictoryPattern = VictoryPattern;
+//# sourceMappingURL=VictoryPattern.js.map
