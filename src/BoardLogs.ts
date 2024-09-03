65ef0e30c0ca156ed6a95fd04bc036a33d380448
hibiki0147 <hibi.king.red@gmail.com>
2024-09-03 22:12:30 +0900

    盤面履歴ドメインの関数作成

--- src/BoardLogs.ts
+++ src/BoardLogs.ts
@@ -2,18 +2,41 @@ import { LineStatus } from "./LineStatus";
 
 export class BoardLogs {
   private boardLogDatas:Array<Array<LineStatus>> = [];
+  private nowLogsIndex:number;
 
   constructor() {
-
+    this.nowLogsIndex = 0;
   }
 
-
-  public undo() {
-    
+  public push(boardDatas:Array<LineStatus>):void {
+    this.boardLogDatas = this.boardLogDatas.slice(0, this.nowLogsIndex + 1);
+    this.boardLogDatas.push([...boardDatas]);
+    this.nowLogsIndex = this.boardLogDatas.length - 1;
   }
 
-  public redo() {
-
+  public undo():Array<LineStatus> | null {
+    if(this.nowLogsIndex > 0) {
+      this.nowLogsIndex--;
+      return this.boardLogDatas[this.nowLogsIndex];
+    } else {
+      return null;
+    }
   }
 
+  public redo():Array<LineStatus> | null {
+    if(this.nowLogsIndex + 1 < boardRecord.length) {
+      this.nowLogsIndex++;
+      return this.boardLogDatas[this.nowLogsIndex];
+    } else {
+      return null;
+    }
+  }
+
+  public getNowBoardData():Array<LineStatus> | null {
+    if(this.nowLogsIndex >= 0 && this.nowLogsIndex < this.boardLogDatas.length) {
+      return this.boardLogDatas[this.nowLogsIndex];
+    } else {
+      return null;
+    }
+  }
 }
